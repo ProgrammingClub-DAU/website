1664a3ef7b5cd5bb19885b01b720108fef8c50d8
madhavthesiya <madhavthesiya07@gmail.com>
2026-08-12 00:08:07 +0530

    fix(navbar): suppress eslint react-hooks/set-state-in-effect rule for hydration guard

--- frontend/src/components/site/navbar.tsx
+++ frontend/src/components/site/navbar.tsx
@@ -46,6 +46,7 @@ export function Navbar() {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setIsMounted(true);
     const onScroll = () => setScrolled(window.scrollY > 8);
     onScroll();
